eb19a25fb07a8bc59322ffe5738edfdc8b5c995d
Muhammad Firmansyah <117868775+Abrajdfk@users.noreply.github.com>
2023-09-21 14:57:47 +0700

    Create Kondisi.py

---
+++ Kondisi.py
@@ -0,0 +1,11 @@
+x = 10
+y = 10
+
+if x > y:
+  print("x lebih besar dari y")
+elif x < y:
+  print("y lebih besar dari x")
+elif x == y:
+  print("x sama dengan y ")
+else:
+  print ("Tidak Diketahui")
