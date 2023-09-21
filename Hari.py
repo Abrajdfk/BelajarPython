0313563f40846ed7c30e5589051a55accfea171e
Muhammad Firmansyah <117868775+Abrajdfk@users.noreply.github.com>
2023-09-21 15:01:40 +0700

    Create Hari.py

---
+++ Hari.py
@@ -0,0 +1,17 @@
+hari_ini = "Minggu"
+
+if (hari_ini == "Senin"):
+   print("Saya Pergi Sekolah")
+elif  (hari_ini == "Selasa"):
+   print("Saya Pergi Sekolah")
+elif  (hari_ini == "Rabu"):
+   print("Saya Pergi Sekolah")
+elif  (hari_ini == "Kamis"):
+   print("Saya Pergi Sekolah")
+elif  (hari_ini == "Jumat"):
+   print("Saya Pergi Sekolah")
+elif  (hari_ini == "Sabtu"):
+   print("Libur")
+elif  (hari_ini == "Minggu"):
+   print("Bobo")
+print(hari_ini)
